7c0ef0fd535d5414933ae19f62333d5780777bff
Michael Telahun <michael.telahun@dindin.et>
2024-03-14 14:03:02 +0300

    The password must be at least 8 characters long

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
+#[derive(Debug)]
+struct PasswordValidator {}
+
+#[derive(Debug)]
+struct ErrorList(Vec<String>);
+
 fn main() {
     println!("Hello, world!");
 }
 
+impl PasswordValidator {
+    pub fn validate(&self, password: &str) -> (bool, ErrorList) {
+        let mut err_list = ErrorList::new();
+        err_list.0.push(String::from("Password must be at least 8 characters"));
+        
+        (false, err_list)
+    }
+}
+
+impl ErrorList {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+}
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::PasswordValidator;
+
+    #[test]
+    fn given_password_when_length_less_than_8_chars_then_error() {
+        // Arrange
+        let pass = "1234567";
+        let validator = PasswordValidator {};
+
+        // Act
+        let (result, errors) = validator.validate(pass);
+
+        // Assert
+        assert_eq!(result, false, "given password length 7, then return false");
+    }
+}
